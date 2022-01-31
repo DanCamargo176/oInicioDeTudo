e47d2175608a64c3a6a12f71f8abce38745885cf
DanCamargo176 <85509633+DanCamargo176@users.noreply.github.com>
2022-01-31 13:48:32 -0300

    Add files via upload
    Adicionei projeto Média Completa

---
+++ Média Completa/Media_calc.php
@@ -0,0 +1,58 @@
+﻿<html
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Média - Daniel Camargo</title>
+</head>
+<body>
+<?php
+include 'form_media.php';
+
+//valores da media
+
+$vl1= $_POST['vl1'];
+if ($vl1>10 || $vl1<0){
+	echo "<center><br> Nota 1: Favor inserir um valor de 0 a 10</center>";
+}
+else{	
+$vl2= $_POST['vl2'];
+if ($vl2>10 || $vl2<0){
+	echo "<center><br> Nota 2: Favor inserir um valor de 0 a 10</center>";
+} else{
+$vl3= $_POST['vl3'];
+if ($vl3>10 || $vl3<0){
+	echo "<center><br> Nota 3: Favor inserir um valor de 0 a 10</center>";
+} else{
+$vl4= $_POST['vl4'];
+if ($vl4>10 || $vl4<0){
+	echo "<center><br> Nota 4: Favor inserir um valor de 0 a 10</center>";
+}
+else{
+
+$media=($vl1+$vl2+$vl3+$vl4)/4;
+$media=number_format($media, '2', '.', '');
+
+if ($media>=7)
+{//inicio do if
+echo "<center> <br> Média:<input name=media type=text value=" .$media."> <br> </center> <hr>";
+echo "<center> <br> Situação:<input name=situacao type=text value= Aprovado> <br> </center>  <hr>";
+}//fim do if
+
+if ($media<7 && $media>5)
+{//inicio do if
+echo "<center> <br> Média:<input name=media type=text value=" .$media."> <br> </center> <hr>";
+echo "<center> <br> Situação: <input name=situacao type=text value= Recuperação> <br> </center> <hr>";
+}//fim do if
+
+if ($media<=5)
+{//inicio do if
+echo "<center> <br> Média:<input name=media type=text value=" .$media."> <br> </center> <hr>";
+echo "<center> <br> Situação: <input name=situacao type=text value= Reprovado> <br> </center> <hr>";
+}//fim do if
+}//fim do $n4
+}//fim do $n3
+}// fim do $n2
+}//fim do else $n1
+?>
+</body>
+
+</html>
---
+++ Média Completa/form_media.php
@@ -0,0 +1,41 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Calculadora - Daniel Camargo</title>
+</head>
+<body>
+<!-- exemplo de formulário-->
+<center>
+
+  
+<form action="Media_calc.php" method="post">
+<table width="300" border="0">
+<tr>
+    <td>Primeira Nota:</td>
+    <td> <input name="vl1" type="number"><br></td>
+  </tr>
+  <tr>
+    <td>Segunda Nota:</td>
+    <td><input name="vl2" type="number"><br></td>
+  </tr>
+  <tr>
+    <td>Terceira Nota:</td>
+    <td><input name="vl3" type="number"><br></td>
+  </tr>
+  <tr>
+    <td>Quarta Nota:</td>
+    <td><input name="vl4" type="number"><br></td>
+  </tr>
+</table>
+ 
+  
+  
+  
+ <input type="submit" name="operacao" value="calcular">
+
+</form>
+<HR/>
+</center>
+</body>                                                                                    
+</html>
